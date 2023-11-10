e8c2659ce8ba01b9739adf85e0cc50f7d17e2ae0
Paulo Marques <pjp.marques@gmail.com>
2023-11-10 21:50:04 +0000

    Updated to display a clock using hardware timers.

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,24 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
 #include <RPi_Pico_TimerInterrupt.h>
+#include <OneButton.h>
 
 #define TFT_BACKLIGHT     4
+#define TFT_CS            22
+#define BUTTON_DN         6
+#define BUTTON_UP         7
+
 #define TIMER_1SEC        1000L     // 1sec = 1000 milliseconds
 
 TFT_eSPI tft;
 RPI_PICO_Timer timer(1);
 
+OneButton up(BUTTON_UP);
+OneButton dn(BUTTON_DN);
+
 volatile unsigned int count = 0;
 volatile int ch = ':';
+volatile uint16_t colour = TFT_WHITE;
 
 bool one_second(struct repeating_timer *t) {
   Serial.println(count);
@@ -25,20 +34,33 @@ bool one_second(struct repeating_timer *t) {
     sprintf(str, "%c%02u", ch, secs);
   }
 
+  tft.setTextColor(colour, TFT_BLACK);
   tft.drawString(str, tft.width()/2, tft.height()/2);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 
   ++count;
   ch = (ch == ':') ? ' ' : ':';
+  colour = TFT_WHITE;
 
   return true;
 }
 
+void up_click() {
+  colour = TFT_BLUE;
+}
+
+void dn_click() {
+  colour = TFT_GREEN;
+}
+
 void setup() {
   Serial.begin(115200);
+  Serial.println("Up and runnning");
 
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(TFT_BACKLIGHT, OUTPUT);
+  pinMode(TFT_CS, OUTPUT);
 
   tft.init();
   tft.setRotation(1);
@@ -47,10 +69,16 @@ void setup() {
   tft.setTextDatum(MC_DATUM);
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
   digitalWrite(TFT_BACKLIGHT, HIGH);
+  digitalWrite(TFT_CS, HIGH);
 
   timer.attachInterruptInterval(TIMER_1SEC * 1000, one_second);
+
+  up.attachClick(up_click);
+  dn.attachClick(dn_click);
 }
  
 void loop() {
-  delay(10000);
+  up.tick();
+  dn.tick();
+  delay(100);
 }
